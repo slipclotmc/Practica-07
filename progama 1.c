193c48f84bcd6393b2ec618002488da6ac9d2393
slipclotmc <slipclotmc8@gmail.com>
2025-11-21 22:24:28 -0600

    Create progama 1.c

---
+++ progama 1.c
@@ -0,0 +1,16 @@
+/* FI UNAM fundamentos de programación
+    Práctica 11 - programa 2c*/
+
+#include<stdio.h>
+int main ()
+{
+ int a, b;
+ a = 3;
+ b = 2;
+ if (a > b)
+ {
+    printf("\ta (%d) es mayor a b (%d).\n",a,b);
+ }
+ printf("\tEl programa sigue su flujo.\n");
+ return 0;
+}
